430baa84f8c77a4a5cdd6754e8d0df04fdf72b1c
dongl <dl978916237@163.com>
2021-07-13 21:44:50 +0800

    1.0.0  es

---
+++ lcn-elasticsearch/src/test/java/com/dongl/es/LcnElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongl.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcnElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
