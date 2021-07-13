989fb8af543010e12142b56e170205b2e44a09fe
dongl <dl978916237@163.com>
2021-07-13 17:25:17 +0800

    main 测试接口

--- lcn-pay/src/main/java/com/dongliang/lcnpay/controller/TestController.java
+++ lcn-pay/src/main/java/com/dongliang/lcnpay/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.dongliang.lcnpay.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author D-L
  * @version 1.0.0
@@ -7,7 +9,7 @@ package com.dongliang.lcnpay.controller;
  * @Description TODO
  * @createTime 2021-05-12 11:18:00
  */
-
+@RestController
 public class TestController {
 
 }
