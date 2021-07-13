3ed2b41bee1fc8030ab8817fe3867142b1a04224
dongl <dl978916237@163.com>
2021-07-13 22:02:58 +0800

    1.0.0 服务测试

--- lcn-elasticsearch/src/main/java/com/dongl/es/LcnElasticsearchApplication.java
+++ lcn-elasticsearch/src/main/java/com/dongl/es/LcnElasticsearchApplication.java
@@ -2,12 +2,23 @@ package com.dongl.es;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class LcnElasticsearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LcnElasticsearchApplication.class, args);
+    }
+
+
+    @GetMapping("/es")
+    public String getEs(){
+        return "es-----------";
     }
 
 }
