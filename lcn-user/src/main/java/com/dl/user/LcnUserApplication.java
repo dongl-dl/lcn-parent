e4f106835fcd25440cab32df6d0d4b0f1989ee63
dongl <dl978916237@163.com>
2021-07-15 18:09:33 +0800

    [main]   Spring Boot 线程池的使用

--- lcn-user/src/main/java/com/dl/user/LcnUserApplication.java
+++ lcn-user/src/main/java/com/dl/user/LcnUserApplication.java
@@ -3,6 +3,10 @@ package com.dl.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,19 @@ public class LcnUserApplication {
         SpringApplication.run(LcnUserApplication.class, args);
     }
 
+    @Bean
+    public ThreadPoolTaskExecutor getThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int cpuNum = Runtime.getRuntime().availableProcessors();
+        int threadNum = cpuNum * 2 + 1;
+        System.out.println(threadNum);
+        executor.setCorePoolSize(threadNum-1);
+        executor.setMaxPoolSize(threadNum);
+        executor.setQueueCapacity(100);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("service-user_");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
 }
---
+++ lcn-user/src/main/java/com/dl/user/controller/AsyncController.java
@@ -0,0 +1,55 @@
+package com.dl.user.controller;
+
+import com.dl.user.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @author D-L
+ * @version 1.0.0
+ * @ClassName AsyncController.java
+ * @Description TODO
+ * @createTime 2021-07-15 16:52:00
+ */
+@RestController
+@RequestMapping("/async")
+public class AsyncController {
+    @Autowired
+    private AsyncService asyncService;
+
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    @GetMapping("/async")
+    public void async(){
+        asyncService.executeAsync();
+    }
+
+    @GetMapping("/async1")
+    public String async1(){
+        Future<String> submit = taskExecutor.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+
+                String data = "success";
+                System.out.println(data);
+                return data;
+            }
+        });
+
+        String data = null;
+        try {
+            data = submit.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
