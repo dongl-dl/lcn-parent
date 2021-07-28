977efd4a85645e1a5184dd44dd58e51b6d393c94
dongl <dl978916237@163.com>
2021-07-29 01:40:28 +0800

    使用redis 的setNx实现分布式锁

--- lcn-lock-redis/src/main/java/com/dongl/redis/controller/RedisLockController.java
+++ lcn-lock-redis/src/main/java/com/dongl/redis/controller/RedisLockController.java
@@ -1,12 +1,14 @@
 package com.dongl.redis.controller;
 
 import com.dongl.redis.lock.RedissonRedLockClient;
+import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +28,8 @@ public class RedisLockController {
     private StringRedisTemplate redisTemplate;
     @Autowired
     private RedissonRedLockClient redLock;
+    @Autowired
+    private Redisson redisson;
 
     /**
      * 初始化设置商品数量
@@ -88,6 +92,71 @@ public class RedisLockController {
             e.printStackTrace();
         } finally {
             redLock.unlock(lockKey); //释放锁
+        }
+        return flag;
+    }
+
+    /**
+     * 使用redis 的setNx (幼儿园级别的分布式锁)
+     * @return
+     */
+    @RequestMapping("/deduct_stock")
+    public String deductStock(){
+        String flag = "success";
+        String lockKey = "P_001";
+        String valueId = UUID.randomUUID().toString();
+        try {
+            //Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, "dongl"); //相当于jedis.setnx(key , value)
+            //redisTemplate.expire(lockKey , 10 , TimeUnit.SECONDS); //不能保证原子性 ,解决办法：用一行命令设置过期时间
+
+            //如果第一个线程执行到10秒 锁自动过期，第二个线程执行到5秒 线程二加的锁被线程一释放，
+            // 这样周而复始及其可能出现前一个线程加的锁立刻就被后面的线程给释放，导致锁永久失效。
+            //解决办法：value值设置唯一值 ， 释放锁之前判断是否为自己线程假的锁。
+            Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, valueId, 10, TimeUnit.SECONDS);
+
+            //这里会存在过期时间大小的设置问题， 解决办法：开启守护线程强行续命
+
+            if (!result) {
+                return "error";
+            }
+            //如果程序执行到这里 redis 服务宕机，而且这把锁没有过期时间，这把锁就永远存在，也就是所谓的死锁。
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(product1Count)); //jedis.get(product1Count)
+            if (stock > 0) {
+                redisTemplate.opsForValue().set(product1Count, (stock - 1) + "");
+            } else {
+                flag = "fail";
+            }
+            System.out.println("result:" + flag + "-----" + stock);
+        }finally {
+            if(valueId.equals(redisTemplate.opsForValue().get(lockKey))){
+                //释放锁
+                redisTemplate.delete(lockKey);
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 使用redisson单机模式实现分布式锁
+     * @return
+     */
+    @RequestMapping("/deduct_stock_redisson")
+    public String deductStockRedisson(){
+        String flag = "success";
+        String lockKey = "P_001";
+
+        RLock lock = redisson.getLock(lockKey);
+        lock.lock(30 , TimeUnit.SECONDS);
+        try {
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(product1Count)); //jedis.get(product1Count)
+            if (stock > 0) {
+                redisTemplate.opsForValue().set(product1Count, (stock - 1) + "");
+            } else {
+                flag = "fail";
+            }
+            System.out.println("result:" + flag + "-----" + stock);
+        }finally {
+            lock.unlock();
         }
         return flag;
     }
