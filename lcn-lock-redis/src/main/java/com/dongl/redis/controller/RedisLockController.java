a30472a2a5089878f9c9bcc6085281058d02d7e9
dongl <dl978916237@163.com>
2021-07-29 11:35:25 +0800

    使用redis 的setNx 实现分布式锁及问题分析

--- lcn-lock-redis/src/main/java/com/dongl/redis/controller/RedisLockController.java
+++ lcn-lock-redis/src/main/java/com/dongl/redis/controller/RedisLockController.java
@@ -1,12 +1,15 @@
 package com.dongl.redis.controller;
 
 import com.dongl.redis.lock.RedissonRedLockClient;
+import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +21,7 @@ import java.util.concurrent.TimeUnit;
  * @createTime 2021-07-28 14:19:00
  */
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/redisson")
 public class RedisLockController {
     private static String product1Count = "product1Count";//商品1的数量key
     private static String lockKey = "testLockKey";//分布式锁的key
@@ -26,13 +29,15 @@ public class RedisLockController {
     private StringRedisTemplate redisTemplate;
     @Autowired
     private RedissonRedLockClient redLock;
+    @Autowired
+    private Redisson redisson;
 
     /**
      * 初始化设置商品数量
      *
      * @return
      */
-    @RequestMapping("/set-product-count")
+    @GetMapping("/set-product-count")
     public String setValue() {
         redisTemplate.opsForValue().set(product1Count, "10");
         return "success";
@@ -43,7 +48,7 @@ public class RedisLockController {
      * 不加锁
      * @return
      */
-    @RequestMapping("/panic-buying")
+    @GetMapping("/panic-buying")
     public String panicBuying() {
         String flag = "success";
         try {
@@ -68,7 +73,7 @@ public class RedisLockController {
      * waitTime:获取锁等待时间 , leaseTime：锁的过期时间
      * @return
      */
-    @RequestMapping("/panic-buying-lock")
+    @GetMapping("/panic-buying-lock")
     public String panicBuyingLock() {
         String flag = "success";
         try {
@@ -88,6 +93,93 @@ public class RedisLockController {
             e.printStackTrace();
         } finally {
             redLock.unlock(lockKey); //释放锁
+        }
+        return flag;
+    }
+
+    /**
+     * 使用 synchronized (jvm锁) 如果你是单体应用  老铁你用jvm锁一点毛病都没有 ， 但是在分布式架构中一眼存在重复扣减的情况
+     * @return
+     */
+    @GetMapping("/deduct_stock")
+    public String deductStock(){
+        String flag = "success";
+
+        synchronized (this) {
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(product1Count)); //jedis.get(product1Count)
+            if (stock > 0) {
+                redisTemplate.opsForValue().set(product1Count, (stock - 1) + "");
+            } else {
+                flag = "fail";
+            }
+            System.out.println("result:" + flag + "-----" + stock);
+        }
+        return flag;
+    }
+
+
+    /**
+     * 使用redis 的setNx (幼儿园级别的分布式锁)
+     * @return
+     */
+    @GetMapping("/deduct_stock_redis")
+    public String deductStockRedis(){
+        String flag = "success";
+        String lockKey = "P_001";
+        String valueId = UUID.randomUUID().toString();
+        try {
+            //Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, "dongl"); //相当于jedis.setnx(key , value)
+            //redisTemplate.expire(lockKey , 10 , TimeUnit.SECONDS); //不能保证原子性 ,解决办法：用一行命令设置过期时间
+
+            //如果第一个线程执行到10秒 锁自动过期，第二个线程执行到5秒 线程二加的锁被线程一释放，
+            // 这样周而复始及其可能出现前一个线程加的锁立刻就被后面的线程给释放，导致锁永久失效。
+            //解决办法：value值设置唯一值 ， 释放锁之前判断是否为自己线程假的锁。
+            Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, valueId, 10, TimeUnit.SECONDS);
+
+            //这里会存在过期时间大小的设置问题， 解决办法：开启守护线程强行续命(实现这一系列操作的框架叫做 redisson )
+
+            if (!result) {
+                return "error";
+            }
+            //如果程序执行到这里 redis 服务宕机，而且这把锁没有过期时间，这把锁就永远存在，也就是所谓的死锁。
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(product1Count)); //jedis.get(product1Count)
+            if (stock > 0) {
+                redisTemplate.opsForValue().set(product1Count, (stock - 1) + "");
+            } else {
+                flag = "fail";
+            }
+            System.out.println("result:" + flag + "-----" + stock);
+        }finally {
+            //解决一个线程释放了不属于自己的锁，导致锁失效
+            if(valueId.equals(redisTemplate.opsForValue().get(lockKey))){
+                //释放锁
+                redisTemplate.delete(lockKey);
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 使用redisson单机模式实现分布式锁
+     * @return
+     */
+    @GetMapping("/deduct_stock_redisson")
+    public String deductStockRedisson(){
+        String flag = "success";
+        String lockKey = "P_001";
+
+        RLock lock = redisson.getLock(lockKey);
+        lock.lock(30 , TimeUnit.SECONDS);
+        try {
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(product1Count)); //jedis.get(product1Count)
+            if (stock > 0) {
+                redisTemplate.opsForValue().set(product1Count, (stock - 1) + "");
+            } else {
+                flag = "fail";
+            }
+            System.out.println("result:" + flag + "-----" + stock);
+        }finally {
+            lock.unlock();
         }
         return flag;
     }
