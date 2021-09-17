e143fbdf6e621ba1abe82046e71343cb153d37d4
dongl <dl978916237@163.com>
2021-09-17 09:28:02 +0800

    支付宝扫码、账号密码支付，微信扫码支付

---
+++ lcn-pay-parent/src/main/java/com/dongl/payparent/LcnPayParentApplication.java
@@ -0,0 +1,13 @@
+package com.dongl.payparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcnPayParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcnPayParentApplication.class, args);
+    }
+
+}
